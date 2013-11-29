dc7ae97cef27f718d9c0acf123a4b5082113467c
pfohjo <pfohjo@gmail.com>
2013-11-29 16:50:02 +0100

    singlestepping syscall
    Conflicts:
    	include/linux/nitro.h

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6059,7 +6059,8 @@ static int __vcpu_run(struct kvm_vcpu *vcpu)
 			r = vcpu_enter_guest(vcpu);
 	
 			if(r > 0 && vcpu->nitro.trap_syscall_hit)
-				r = nitro_handle_syscall_trap(vcpu);
+				//r = nitro_handle_syscall_trap(vcpu);
+				nitro_handle_syscall_trap(vcpu);
 		else {
 			srcu_read_unlock(&kvm->srcu, vcpu->srcu_idx);
 			kvm_vcpu_block(vcpu);
